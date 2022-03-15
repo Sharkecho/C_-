369883d4a19cd62ad6e57acf823756e2652e6ebd
Echo <6783317@gmail.com>
2022-03-15 08:24:29 +0800

    测试第一次

---
+++ 01/Project1/01.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_ND_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+/*------------------------------------------------------------*/
+test01()
+{
+
+}
+int main(void)
+{
+	printf("hello,world\n"),
+	system("pause");
+	return EXIT_SUCCESS;
+}
